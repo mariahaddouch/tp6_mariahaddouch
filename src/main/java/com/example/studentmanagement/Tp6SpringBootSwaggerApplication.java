4e683ab7b379045d653090e37d605266f72c4f17
mariahaddouch <128851286+mariahaddouch@users.noreply.github.com>
2025-10-23 20:52:49 +0100

    TP6 Maria Haddouch

---
+++ src/main/java/com/example/studentmanagement/Tp6SpringBootSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp6SpringBootSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp6SpringBootSwaggerApplication.class, args);
+    }
+
+}
